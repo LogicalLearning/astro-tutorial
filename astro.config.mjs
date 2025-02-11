497728a6ebe4c91b523cecb59256c50bcf359d1c
Steve Tate <srtate@uncg.edu>
2025-02-11 09:48:49 -0500

    First try katex integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
-});
+  integrations: [preact()],
+
+	//
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [
+			[
+				rehypeKatex,
+				{
+					// Katex plugin options
+				}
+			]
+		]
+	}
+})
+
--- src/blog/post-3.md
+++ src/blog/post-3.md
@@ -9,3 +9,10 @@ pubDate: 2022-07-15
 tags: ["astro", "learning in public", "setbacks", "community"]
 ---
 It wasn't always smooth sailing, but I'm enjoying building with Astro. And, the [Discord community](https://astro.build/chat) is really friendly and helpful!
+
+Writing some $e^x$ math
+
+$$ \sum_{i=0}^n i^2 $$
+
+That's a display formula
+
